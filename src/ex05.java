1dc2bf1fdace9fd9e85b9da751f5f3951af41cc1
lucasyoshi3 <lucasyoshihirosm@gmail.com>
2023-03-31 01:18:16 -0300

    correção ex05 sem for

--- src/ex05.java
+++ src/ex05.java
@@ -4,16 +4,23 @@ public class ex05 {
         Scanner e=new Scanner(System.in);
         System.out.println("Insira o valor de n: ");
         int n=e.nextInt();
-        System.out.println(serie5(n));
+        int soma=0;
+        int numeroMax=n;
+        System.out.println(serie5(n,soma,numeroMax));
     }
-    public static int serie5(int n){
-        int i;
-        int fat=n;
-        for(i=n-1;i>=1;i--){
-            fat=fat*i;
+    public static int serie5(int n,int soma,int numeroMax) {
+        if (numeroMax != 1) {
+            int fat=fatorial(n);
+            n=numeroMax-1;
+            soma=fat+serie5(n,soma,numeroMax-1);
+            return soma;
+        } else{
+            return 1;
         }
+    }
+    public static int fatorial(int n){
         if (n!=1){
-            n=fat+serie5(n-1);
+            n=n*fatorial(n-1);
             return n;
         }else{
             return 1;
